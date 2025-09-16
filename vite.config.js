5e068e16ce6e1dee7dc652b6f90f0fda92d41a94
Shane <6071159+smashedr@users.noreply.github.com>
2025-09-16 12:40:04 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
+const name = 'Contributors'
+
 export default defineConfig({
     plugins: [vue()],
     build: {
         lib: {
-            entry: path.resolve(__dirname, 'src/Contributors.vue'),
-            name: 'Contributors',
-            fileName: (format) => `Contributors.${format}.js`,
+            entry: path.resolve(__dirname, `src/${name}.vue`),
+            name,
+            fileName: (format) => `${name}.${format}.js`,
         },
         cssCodeSplit: true,
         rollupOptions: {
