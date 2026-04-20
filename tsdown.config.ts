41223800c326af728d47ebf21f09fb79bd586c55
Shane <6071159+smashedr@users.noreply.github.com>
2026-04-19 20:21:01 -0700

    Restore Shebang Line

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsdown'
 // noinspection JSUnusedGlobalSymbols
 export default defineConfig({
   entry: ['src/get-contributors.ts'],
-  banner: { js: '#!/usr/bin/env node' },
+  // banner: { js: '#!/usr/bin/env node' },
   outDir: 'dist',
   minify: true,
   clean: false,
