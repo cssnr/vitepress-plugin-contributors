0541fa7ae0122fd454ad98ec5caf4a25c01b8df4
Shane <6071159+smashedr@users.noreply.github.com>
2026-04-19 17:32:11 -0700

    tsdown.config.ts

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsdown'
 // noinspection JSUnusedGlobalSymbols
 export default defineConfig({
   entry: ['src/get-contributors.ts'],
+  banner: { js: '#!/usr/bin/env node' },
   outDir: 'dist',
   minify: true,
   clean: false,
